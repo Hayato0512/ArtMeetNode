615ea60e3c7368913b1232512e08d264a4a45b13
Hayato Koyama <hayato@formswim.com>
2023-08-02 07:51:32 -0700

    insert class

--- routes/userQueries.js
+++ routes/userQueries.js
@@ -102,6 +102,35 @@ router.post("/insertevent", (req, res) => {
  
 });
 
+router.post("/insertclass", (req, res) => {
+  var hostId = req.body.hostId
+  var title = req.body.title
+  var desc = req.body.desc
+  var imageUrl = req.body.imageUrl
+  var date = req.body.date
+  var duration = req.body.duration
+  var numOfSpots = req.body.numOfSpots
+  var location  = req.body.location 
+  var eventType  = req.body.eventType 
+  
+  try {
+    connection.query(
+      `INSERT INTO class (hostId, title, desc2, imageUrl, date, duration, numOfSpots, location, eventType) VALUES (${hostId},'${title}','${desc}', '${imageUrl}', '${date}', ${duration}, ${numOfSpots}, '${location}', ${eventType})`,
+      (error, res2) => {
+        if (error) throw error;
+  
+        if (!error) {
+          console.log("succeed");
+          res.send("succeed");
+        }
+      }
+    );
+  } catch (error) {
+    
+  }
+ 
+});
+
 router.delete("/deletefirst", async (req, res) => {
   console.log("delete query called");
   try {
